52b7cdfc8ecbbbff5ddebe2f4ba4ebfc1d279422
oklukaan <102958563+oklukaan@users.noreply.github.com>
2022-08-30 11:21:19 +0300

    ilk

---
+++ assets/code.js
@@ -0,0 +1,75 @@
+/*
+        var Btn=document.querySelector('#Btn');
+        var control=document.querySelector('#accept');
+        var control1=document.querySelector('#accept2');
+        var control1=document.querySelector('#accept3');
+        var diz= document.querySelectorAll("#oldu");
+        var diz1=document.querySelectorAll("#olmadi");
+
+        Btn.addEventListener('click', () =>{
+        
+          
+        if(control.checked){
+              
+
+        for(var i =0; i<diz.length;i++){
+              diz[i].style.display="none";
+            }
+            
+        }
+          else{
+            for(var i =0; i<diz.length;i++){
+              diz[i].style.display="block";
+            }
+          }
+        
+
+        });
+        Btn.addEventListener('click', () =>{
+        
+          
+        if(control1.checked){
+              
+
+        for(var i =0; i<diz1.length;i++){
+              diz1[i].style.display="none";
+            }
+            
+        }
+          else{
+            for(var i =0; i<diz1.length;i++){
+              diz1[i].style.display="block";
+            }
+          }
+        
+
+        });
+        Btn.addEventListener('click', () =>{
+
+  
+          
+          if(control.checked && control1.checked){
+            for(var i=0;i<diz.length;i++){
+              diz[i].style.display="block";
+            }
+            for(var i=0;i<diz1.length;i++){
+              diz1[i].style.display="block";
+            }
+          }
+        
+          
+
+        });
+
+*/
+        
+
+          
+         
+
+
+
+
+
+
+        
